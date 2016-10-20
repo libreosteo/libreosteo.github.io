4e606d5ffd76771c6bdec4c7689e2ac80b046ce3
Garthylou Jean-Baptiste Gury <garthylou@gmx.com>
2016-10-20 14:25:06 +0200

    Update for the version 0.5.4

--- js/downloader.js
+++ js/downloader.js
@@ -1,6 +1,6 @@
-var windowsLocation = "https://github.com/libreosteo/Libreosteo/releases/download/0.5.3/Libreosteo-0.5.3-win32.zip";
-var linuxLocation = "https://github.com/libreosteo/Libreosteo/archive/0.5.3.tar.gz";
-var macosLocation = "https://github.com/libreosteo/Libreosteo/releases/download/0.5.3/Libreosteo.pkg.zip";
+var windowsLocation = "https://github.com/libreosteo/Libreosteo/releases/download/0.5.4/Libreosteo-0.5.4-win32.zip";
+var linuxLocation = "https://github.com/libreosteo/Libreosteo/archive/0.5.4.tar.gz";
+var macosLocation = "https://github.com/libreosteo/Libreosteo/releases/download/0.5.4/Libreosteo.pkg.zip";
 
 var windowsLocationBeta = "https://github.com/libreosteo/Libreosteo/releases/download/0.4.9.2/Libreosteo-beta-v0.4.9.2-win32.zip";
 var linuxLocationBeta = "https://github.com/libreosteo/Libreosteo/archive/0.4.9.2.tar.gz";
