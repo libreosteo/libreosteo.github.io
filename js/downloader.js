f4f24b879e0bb4f7fc4930fd3f8ec3918962c833
Jean-Baptiste G <jean-baptiste.gury@cambiatech.com>
2022-01-19 08:25:18 +0100

    Update downloader.js

--- js/downloader.js
+++ js/downloader.js
@@ -1,4 +1,4 @@
-var windowsAMD64Location = "https://github.com/libreosteo/Libreosteo/releases/download/0.6.1/Libreosteo-0.6.2-win64.zip";
+var windowsAMD64Location = "https://github.com/libreosteo/Libreosteo/releases/download/0.6.2/Libreosteo-0.6.2-win64.zip";
 var windowsLocation = windowsAMD64Location;
 var linuxLocation = "https://github.com/libreosteo/Libreosteo/archive/0.6.2.tar.gz";
 var macosLocation = "https://github.com/libreosteo/Libreosteo/releases/download/0.6.2/Libreosteo.mpkg.zip";
